7a20def0eea6923e56e6b283f4c770527396db54
Julien GARDET (aka. Jitrixis) <julien.gardet@jitrixis.net>
2015-06-02 17:52:37 +0200

    create api iface selection

--- api.py
+++ api.py
@@ -2,3 +2,13 @@ __author__ = 'jitrixis'
 
 from scapy.all import *
 from Fatory.all import *
+import re
+
+
+def __select_iface():
+    pass
+
+
+'''TODO: Select un interface'''
+iface = ''
+__select_iface()
