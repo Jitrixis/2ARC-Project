22474ba1cb6ec91ed711c42efc8b4679439a357b
Julien GARDET (aka. Jitrixis) <julien.gardet@jitrixis.net>
2015-06-11 03:56:54 +0200

    init ICMP packet

--- Packet/ICMP.py
+++ Packet/ICMP.py
@@ -1,1 +1,10 @@
 __author__ = 'jitrixis'
+
+class Icmp:
+    def __init__(self):
+        self.__type = 8
+        self.__code = 0
+        self.__checksum = 0x0
+        self.__id = 0x0
+        self.__seq = 0x0
+
